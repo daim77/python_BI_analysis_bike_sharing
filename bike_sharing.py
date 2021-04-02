63408d96a6719a7e382e72052394835a7b128703
Martin Danek <martindanek4@gmail.com>
2021-04-03 00:05:09 +0200

    SQL update, basic stat, notes

--- bike_sharing.py
+++ bike_sharing.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 
 import sqlalchemy as db
@@ -29,7 +28,32 @@ def load_data_from_engeto():
 
     db_connection.close()
 
+    weather_df.drop(weather_df.loc[weather_df['date'] < '2018-09-15'].index,
+                    inplace=True)
+    weather_df.drop(weather_df.loc[weather_df['date'] == '2020-10-31'].index,
+                    inplace=True)
     write_data_to_csv(bikes_df, weather_df)
+
+    user = "lamikoko.cz.1"
+    password = "Zzdenka1"
+    conn_string = f"mysql+pymysql://{user}:{password}@lamikoko.cz/lamikokocz1"
+
+    lamikoko_conn = db.create_engine(conn_string, echo=True)
+    bikes_df.to_sql('bikes', lamikoko_conn)
+    weather_df.to_sql('weather', lamikoko_conn)
+
+    return bikes_df, weather_df
+
+
+def load_from_lamikoko():
+    user = "lamikoko.cz.1"
+    password = "Zzdenka1"
+    conn_string = f"mysql+pymysql://{user}:{password}@lamikoko.cz/lamikokocz1"
+
+    lamikoko_conn = db.create_engine(conn_string, echo=True)
+    bikes_df = pd.read_sql('bikes', lamikoko_conn, parse_dates=True)
+    weather_df = pd.read_sql('weather', lamikoko_conn, parse_dates=True)
+
     return bikes_df, weather_df
 
 
@@ -39,30 +63,42 @@ def write_data_to_csv(bikes_df, weather_df):
     return
 
 
-def data_description(bike_df, weather_df):
+def data_stat(bikes_df, weather_df):
     pd.set_option('display.max_rows', 500)
     pd.set_option('display.max_columns', 500)
     pd.set_option('display.width', 1000)
 
     print('=' * 50)
-    print(bike_df.columns)
-    print(bike_df.describe())
-    print(bike_df.head(10))
+    print(bikes_df.columns)
+    print(bikes_df.head(10))
     print('=' * 50)
     print(weather_df.columns)
-    print(weather_df.describe())
     print(weather_df.head(10))
+
+    stations_df = bikes_df['start_station_id'].value_counts()
+    print('Number of stations',
+          stations_df.index.__len__())
+    print('Station used less then 50 times',
+          stations_df.loc[stations_df < 50].index.__len__())
+
+    print(bikes_df.started_at.min())
+    print(bikes_df.started_at.max())
+
+
+
+def compute_data(bikes_df, weather_df):
+    bikes_df['duration'] = bikes_df['ended_at'] - bikes_df['started_at']
+    bikes_df['duration'] = bikes_df['duration'].dt.seconds
+    return
 
 
 def main():
-    if not os.path.isfile('tables/bikes_df.csv') or \
-            not os.path.isfile('tables/weather_df.csv'):
-        bikes_df, weather_df = load_data_from_engeto()
-    else:
-        bikes_df = pd.read_csv('tables/bikes_df.csv')
-        weather_df = pd.read_csv('tables/weather_df.csv')
+    # bikes_df, weather_df = load_data_from_engeto()
+    bikes_df, weather_df = load_from_lamikoko()
 
-    data_description(bikes_df, weather_df)
+    data_stat(bikes_df, weather_df)
+
+    compute_data(bikes_df, weather_df)
 
 
 if __name__ == '__main__':
