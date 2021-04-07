dba3f18d1abe2a7d1a74021c620687bc0cce4bed
Martin Danek <martindanek4@gmail.com>
2021-04-07 23:11:28 +0200

    plot

--- bike_sharing.py
+++ bike_sharing.py
@@ -1,3 +1,4 @@
+import datetime
 import pandas as pd
 
 import sqlalchemy as db
@@ -34,9 +35,9 @@ def load_data_from_engeto():
                     inplace=True)
     write_data_to_csv(bikes_df, weather_df)
 
-    user = "lamikoko.cz.1"
+    user = "lamikoko.cz.2"
     password = "Zzdenka1"
-    conn_string = f"mysql+pymysql://{user}:{password}@lamikoko.cz/lamikokocz1"
+    conn_string = f"mysql+pymysql://{user}:{password}@lamikoko.cz/lamikokocz2"
 
     lamikoko_conn = db.create_engine(conn_string, echo=True)
     bikes_df.to_sql('bikes', lamikoko_conn)
@@ -46,9 +47,9 @@ def load_data_from_engeto():
 
 
 def load_from_lamikoko():
-    user = "lamikoko.cz.1"
+    user = "lamikoko.cz.2"
     password = "Zzdenka1"
-    conn_string = f"mysql+pymysql://{user}:{password}@lamikoko.cz/lamikokocz1"
+    conn_string = f"mysql+pymysql://{user}:{password}@lamikoko.cz/lamikokocz2"
 
     lamikoko_conn = db.create_engine(conn_string, echo=True)
     bikes_df = pd.read_sql('bikes', lamikoko_conn, parse_dates=True)
@@ -75,20 +76,50 @@ def data_stat(bikes_df, weather_df):
     print(weather_df.columns)
     print(weather_df.head(10))
 
-    stations_df = bikes_df['start_station_id'].value_counts()
+    stations_starts_df = bikes_df['start_station_id'].value_counts()
+    stations_ends_df = bikes_df['end_station_id'].value_counts()
+
+    print('Nejvice se akumuluje: ',
+          (stations_ends_df - stations_starts_df).idxmax())
+
     print('Number of stations',
-          stations_df.index.__len__())
+          stations_starts_df.index.__len__())
     print('Station used less then 50 times',
-          stations_df.loc[stations_df < 50].index.__len__())
+          stations_starts_df.loc[stations_starts_df < 50].index.__len__())
 
     print(bikes_df.started_at.min())
     print(bikes_df.started_at.max())
 
 
-
 def compute_data(bikes_df, weather_df):
     bikes_df['duration'] = bikes_df['ended_at'] - bikes_df['started_at']
     bikes_df['duration'] = bikes_df['duration'].dt.seconds
+    return
+
+
+def bikes_movement(bikes_df):
+    date_transfer = bikes_df.started_at.dt.date.min()
+    period_df = bikes_df.started_at.dt.date.max() - bikes_df.started_at.dt.date.min()
+    period = period_df.days
+    for i in range(1, period + 1):
+
+        mov_start_df = \
+            bikes_df.loc[bikes_df['started_at']
+                             .dt.date <= date_transfer, ['start_station_id']]\
+                .value_counts()
+
+        mov_end_df = \
+            bikes_df.loc[bikes_df['started_at'].
+                             dt.date <= date_transfer, ['end_station_id']]\
+                .value_counts()
+
+        diff_df = mov_end_df - mov_start_df
+
+        if diff_df.max() > 10:
+            print('diff_df_max: ', diff_df.idxmax)
+
+        date_transfer += datetime.timedelta(days=i)
+
     return
 
 
